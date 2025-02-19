40ea0d83cba3f01c8c8c367aed7f544308b192d9
David Henrique <dh210605@gmail.com>
2025-02-18 21:32:22 -0300

    [ feature ]:  Scanner, int, double

---
+++ Exemplo2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Exemplo2 {
+
+public static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int num1;
+        int num2;
+
+        System.out.print("Digita o numero 1 aq -> ");
+        num1 = input.nextInt();
+        
+        System.out.print("Digita o numero 2 aq -> ");
+        num2 = input.nextInt();
+
+        int total = num1 + num2;
+        
+        System.out.println("Total - > "+ total);
+
+    }
+}
+
+
+
